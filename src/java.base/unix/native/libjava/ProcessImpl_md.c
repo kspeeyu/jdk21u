e7eab1433f940b44492b935f743336cdfe5b3b75
Jenny Shivayogi <kspeeyu@amazon.com>
2023-07-07 13:11:12 -0700

    freeing the buf before return

--- src/java.base/unix/native/libjava/ProcessImpl_md.c
+++ src/java.base/unix/native/libjava/ProcessImpl_md.c
@@ -558,7 +558,8 @@ spawnChild(JNIEnv *env, jobject process, ChildStuff *c, const char *helperpath) 
 
     /* write the two structs and the data buffer */
     if (writeFully(c->childenv[1], (char *)&magic, sizeof(magic)) != sizeof(magic)) { // magic number first
-        return -1;
+        free(buf);
+	return -1;
     }
 #ifdef DEBUG
     jtregSimulateCrash(resultPid, 2);
@@ -566,12 +567,12 @@ spawnChild(JNIEnv *env, jobject process, ChildStuff *c, const char *helperpath) 
     if (writeFully(c->childenv[1], (char *)c, sizeof(*c)) != sizeof(*c) ||
         writeFully(c->childenv[1], (char *)&sp, sizeof(sp)) != sizeof(sp) ||
         writeFully(c->childenv[1], buf, bufsize) != bufsize) {
-        return -1;
+	free(buf);        
+	return -1;
     }
     /* We're done. Let jspwanhelper know he can't expect any more data from us. */
     close(c->childenv[1]);
     c->childenv[1] = -1;
-    free(buf);
 #ifdef DEBUG
     jtregSimulateCrash(resultPid, 3);
 #endif
